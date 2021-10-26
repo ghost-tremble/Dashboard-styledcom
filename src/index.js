58892e450b836598bde9ca26f9d0436b1b4496c6
ghost-tremble <77670549+ghost-tremble@users.noreply.github.com>
2021-10-26 16:31:55 +0100

    all external files deleted only portfolio will be pushed

--- src/index.js
+++ src/index.js
@@ -1,19 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
-import App from "./Styledcomponents/App";
-//import { AppProvider } from "./Revision/Contexts/Context";
+import App from "./Patrick/App";
+// import App from "./Framer/src/App";
+// import "./Framer/src/index.css";
 
 let renderable = document.getElementById("root");
 
 const Main = () => {
-    return ( <
-        React.StrictMode >
-        <
-        App / >
-        <
-        /React.StrictMode>
-    );
+  return <App />;
 };
 
-ReactDOM.render( < Main / > , renderable);
+ReactDOM.render(
+  <>
+    <Main />{" "}
+  </>,
+  renderable
+);
